39103b4f5d6ce6705217232758099f494d0f0473
ravis <ravis@DESKTOP-MO95S8H>
2020-09-03 19:32:31 +0530

    2nd version

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Please refer to the message!
             Console.WriteLine("Hello World!");
         }
     }
